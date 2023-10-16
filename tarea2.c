c53dafcb13ed8ab247a4a21c310c525b0f69b6ed
Lucas Serrano Jiménez <lucasserrano2002@gmail.com>
2023-10-16 16:33:28 +0200

    clase

--- tarea2.c
+++ tarea2.c
@@ -1,62 +1,65 @@
 #include <stdio.h>
+#include <stdint.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+const int A = 7;
+
+void swap(int *stack_a, int *stack_b) {
+    int stack_temp = *stack_a;
+    *stack_a = *stack_b;
+    *stack_b = stack_temp;
 }
 
-void sortMatrix(int mat[3][3]) {
-    int tempArr[9];
-    int k = 0;
+void sortMatrix(int stack_mat[3][3]) {
+    int stack_tempArr[9];
+    int stack_k = 0;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            tempArr[k++] = mat[i][j];
+    for (int heap_i = 0; heap_i < 3; heap_i++) {
+        for (int heap_j = 0; heap_j < 3; heap_j++) {
+            stack_tempArr[stack_k++] = stack_mat[heap_i][heap_j];
         }
     }
 
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (tempArr[j] > tempArr[j + 1]) {
-                swap(&tempArr[j], &tempArr[j + 1]);
+    for (int heap_i = 0; heap_i < 9; heap_i++) {
+        for (int heap_j = 0; heap_j < 8; heap_j++) {
+            if (stack_tempArr[heap_j] > stack_tempArr[heap_j + 1]) {
+                swap(&stack_tempArr[heap_j], &stack_tempArr[heap_j + 1]);
             }
         }
     }
 
-    k = 0;
+    stack_k = 0;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            mat[i][j] = tempArr[k++];
+    for (int heap_i = 0; heap_i < 3; heap_i++) {
+        for (int heap_j = 0; heap_j < 3; heap_j++) {
+            stack_mat[heap_i][heap_j] = stack_tempArr[stack_k++];
         }
     }
 }
 
+int8_t suma(int stack_mat[3][3]){
+    int8_t stack_sum = 0;
+    for (int heap_i = 0; heap_i < 3; heap_i++) {
+        for (int heap_j = 0; heap_j < 3; heap_j++) {
+            stack_sum+=stack_mat[heap_i][heap_j];
+        }
+    }
+    return stack_sum;
+}
+
 int main() {
-    int matrix[3][3] = {
+    int ELF_st_matrix[3][3] = {
         {4, 5, 7},
         {8, 1, 6},
         {3, 2, 9}
     };
-
-    printf("Matriz original:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    sortMatrix(matrix);
-
-    printf("\nMatriz ordenada:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
+    auto ELF_auto_sumaContenido = 0;
+    long st_rperez_a;
+    
+    sortMatrix(ELF_st_matrix);
+    ELF_auto_sumaContenido = suma(ELF_st_matrix);
+    printf("%d \n",ELF_auto_sumaContenido);
+	
+    
 
     return 0;
 }
